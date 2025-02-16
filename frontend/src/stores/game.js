8ff6dcf1423abeaac9583f4a73e7fea3ea1d0aa7
Ray Luna <leon.luna.ray@gmail.com>
2025-02-15 17:33:39 -0800

    game store

--- frontend/src/stores/game.js
+++ frontend/src/stores/game.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { defineStore } from 'pinia';
 
 export const useGameStore = defineStore('game', () => {
     const data = ref(null);
 
-    const fetchRandomStory = async () => {
-        const response = await axios.get('/api/game/random');
-        data.value = response.data;
-    }
+    // const fetchRandomStory = async () => {
+    //     const response = await axios.get('/api/game/random');
+    //     data.value = response.data;
+    // }
 
     return {
         data,
