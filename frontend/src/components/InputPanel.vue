85d530f7e167c34eaa0e0beca6cc8c99c8d42648
Ray Luna <leon.luna.ray@gmail.com>
2025-02-16 11:13:56 -0800

    update python

--- frontend/src/components/InputPanel.vue
+++ frontend/src/components/InputPanel.vue
@@ -1,10 +1,12 @@
 <template>
     <section class="flex justify-center items-center h-full">
         <div v-if="inputs.length" class="flex-col-2 justify-cetner items-center lg:w-[75%]">
-            <div class="border p-[4rem] rounded-md text-center flex-col-2">
-                <label class="h2">{{ inputs[activeIndex] }}</label>
-                <input type="text" v-model="userInput" class="text-[1.25rem] font-nunito"/>
-            </div>
+            <form class="p-[4rem] rounded-md text-center grid md:grid-cols-2 gap-[1rem]">
+                <div class="form-input border p-[2rem]" v-for="(input, index) in inputs" :key="index">
+                    <label class="h2">{{ inputs[index] }}</label>
+                    <input type="text" v-model="userInput" class="text-[1.25rem] font-nunito" required />
+                </div>
+            </form>
             <button class="btn w-full uppercase text-center" @click="nextInput">Next</button>
         </div>
     </section>
